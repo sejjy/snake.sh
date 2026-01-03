0e0dbce5133bc6eddf678c4bdec1568d80a0f119
sejjy <sejjymvm@gmail.com>
2026-01-03 23:48:49 +0800

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+<div align="center">
+
+## 🐍 snake.sh
+
+Snake written in Bash.
+
+| [Preview](./preview.gif) |
+| :----------------------: |
+
+</div>
+
+#
+
+### Play
+
+To play without downloading, run:
+
+```bash
+bash <(curl -s https://raw.githubusercontent.com/sejjy/snake.sh/main/snake.sh)
+```
+
+Otherwise, clone the repository and run `./snake.sh`:
+
+```bash
+git clone https://github.com/sejjy/snake.sh.git && cd snake.sh && ./snake.sh
+```
+
+#
+
+### References
+
+- [ANSI Escape Sequences](https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797)
+- [snake-term](https://github.com/abuharth/snake-term)
---
+++ snake.sh
@@ -0,0 +1,286 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+#
+# snake.sh: Snake written in Bash.
+#
+
+DELAY=0.15
+GRID_HEIGHT=20
+GRID_WIDTH=20
+
+KEY_U="w"
+KEY_L="a"
+KEY_D="s"
+KEY_R="d"
+
+CHAR_HEAD="O"
+CHAR_BODY="o"
+CHAR_TILE="."
+CHAR_APPLE="@"
+
+WALL_H="─"
+WALL_V="│"
+WALL_TL="┌"
+WALL_TR="┐"
+WALL_BL="└"
+WALL_BR="┘"
+
+FG_RED="\e[31m"
+FG_GREEN="\e[32m"
+FG_YELLOW="\e[33m"
+FG_BLUE="\e[34m"
+FG_GRAY="\e[90m"
+FG_RESET="\e[39m"
+
+tput() {
+	case $1 in
+		"smcup") printf "\e[?1049h" ;;
+		"rmcup") printf "\e[?1049l" ;;
+		"civis") printf "\e[?25l" ;;
+		"cnorm") printf "\e[?25h" ;;
+		"cup")
+			printf "\e[%d;%dH" $(($3 + OFFSET_Y + 1)) $(($2 + OFFSET_X + 1))
+			;;
+	esac
+}
+
+draw_char() {
+	tput cup $(($1 * 2 + 2)) $(($2 + 1))
+	case $3 in
+		"$CHAR_HEAD")  printf "%b%s%b" "$FG_BLUE" "$3" "$FG_RESET" ;;
+		"$CHAR_BODY")  printf "%b%s%b" "$FG_BLUE" "$3" "$FG_RESET" ;;
+		"$CHAR_APPLE") printf "%b%s%b" "$FG_RED"  "$3" "$FG_RESET" ;;
+		"$CHAR_TILE")  printf "%b%s%b" "$FG_GRAY" "$3" "$FG_RESET" ;;
+	esac
+}
+
+display_score() {
+	if (($# == 0)); then tput cup $CENTER_X -1; fi
+	printf "score: %03d     highscore: %03d" $score $highscore
+}
+
+display_header() {
+	local header=(
+		"█▀▀ █▀█ █▀█ █ █ █▀▀   █▀▀ █ █"
+		"▀▀█ █ █ █▀█ █▀▄ █▀▀   ▀▀█ █▀█"
+		"▀▀▀ ▀ ▀ ▀ ▀ ▀ ▀ ▀▀▀ ▀ ▀▀▀ ▀ ▀"
+	)
+	local i
+	for ((i = 0; i < ${#header[@]}; i++)); do
+		tput cup $CENTER_X $((-5 + i))
+		printf "%s" "${header[$i]}"
+	done
+}
+
+draw_walls() {
+	local line
+	printf -v line "%$((CANVAS_WIDTH - 2))s" ""
+
+	local top=$WALL_TL top+=${line// /$WALL_H} top+=$WALL_TR
+	local mid=$WALL_V  mid+=$line              mid+=$WALL_V
+	local bot=$WALL_BL bot+=${line// /$WALL_H} bot+=$WALL_BR
+
+	tput cup 0 0
+	printf "%b%s%b" "$FG_GRAY" "$top" "$FG_RESET"
+
+	local y
+	for ((y = 1; y < CANVAS_HEIGHT - 1; y++)); do
+		tput cup 0 $y
+		printf "%b%s%b" "$FG_GRAY" "$mid" "$FG_RESET"
+	done
+
+	tput cup 0 $((CANVAS_HEIGHT - 1))
+	printf "%b%s%b" "$FG_GRAY" "$bot" "$FG_RESET"
+}
+
+display_footer() {
+	local footer=(
+		"↑ ← ↓ →              ^C"
+		"$KEY_U $KEY_L $KEY_D $KEY_R: move       esc: exit"
+	)
+	local i
+	for ((i = 0; i < ${#footer[@]}; i++)); do
+		tput cup $CENTER_X $((CANVAS_HEIGHT + i))
+		printf "%s" "${footer[$i]}"
+	done
+}
+
+update_apple_pos() {
+	# make sure the apple spawns in an empty tile
+	while true; do
+		apple_x=$((RANDOM % GRID_WIDTH))
+		apple_y=$((RANDOM % GRID_HEIGHT))
+
+		# avoid the head
+		if ((apple_x == head_x && apple_y == head_y)); then continue; fi
+
+		local is_empty=true
+		# avoid the body
+		local i
+		for ((i = 0; i < ${#body_x[@]}; i++)); do
+			if ((apple_x == body_x[i] && apple_y == body_y[i])); then
+				is_empty=false
+				break
+			fi
+		done
+
+		if $is_empty; then break; fi
+	done
+
+	draw_char $apple_x $apple_y "$CHAR_APPLE"
+}
+
+validate_move() {
+	# check wall collision
+	if ((head_x < 0 || head_x >= GRID_WIDTH)) ||
+		((head_y < 0 || head_y >= GRID_HEIGHT)); then
+		return 1
+	fi
+
+	# check self collision
+	local i
+	for ((i = 0; i < ${#body_x[@]}; i++)); do
+		if ((head_x == body_x[i] && head_y == body_y[i])); then return 1; fi
+	done
+
+	is_eaten=false
+	# check if the apple is eaten
+	if ((head_x == apple_x && head_y == apple_y)); then
+		is_eaten=true
+		score=$((score + 1))
+
+		body_x+=("${body_x[-1]}")
+		body_y+=("${body_y[-1]}")
+
+		update_apple_pos
+		display_score
+	fi
+}
+
+update_snake_pos() {
+	prev_tail_x=${body_x[-1]}
+	prev_tail_y=${body_y[-1]}
+
+	local i
+	for ((i = ${#body_x[@]} - 1; i > 0; i--)); do
+		body_x[i]=${body_x[i - 1]}
+		body_y[i]=${body_y[i - 1]}
+	done
+
+	body_x[0]=$prev_head_x
+	body_y[0]=$prev_head_y
+
+	if ! $is_eaten; then
+		# do not draw if the apple is at the previous tail position
+		if ((prev_tail_x != apple_x || prev_tail_y != apple_y)); then
+			draw_char "$prev_tail_x" "$prev_tail_y" "$CHAR_TILE"
+		fi
+	fi
+	draw_char $prev_head_x $prev_head_y "$CHAR_BODY"
+	draw_char $head_x $head_y "$CHAR_HEAD"
+}
+
+handle_input() {
+	# accept arrow keys
+	local reply
+	if [[ $REPLY == $'\e' ]]; then
+		read -rn 2 -t 0.001 reply
+		REPLY+=$reply
+	fi
+
+	case $REPLY in
+		$'\e[A' | "$KEY_U") if ((dir_y != 1));  then dir_x=0  dir_y=-1; fi ;;
+		$'\e[B' | "$KEY_D") if ((dir_y != -1)); then dir_x=0  dir_y=1;  fi ;;
+		$'\e[C' | "$KEY_R") if ((dir_x != -1)); then dir_x=1  dir_y=0;  fi ;;
+		$'\e[D' | "$KEY_L") if ((dir_x != 1));  then dir_x=-1 dir_y=0;  fi ;;
+		$'\e') exit 0 ;;
+	esac
+}
+
+init_game() {
+	if ((score > highscore)); then highscore=$score; fi
+	score=0
+	display_score
+
+	head_x=$((GRID_WIDTH / 4))
+	head_y=$((GRID_HEIGHT / 2))
+	body_x=($((head_x - 1)) $((head_x - 2)))
+	body_y=($((head_y)) $((head_y)))
+	apple_x=$((GRID_WIDTH / 2))
+	apple_y=$((GRID_HEIGHT / 2))
+
+	dir_x=0
+	dir_y=0
+
+	local tiles
+	printf -v tiles "%$((GRID_WIDTH))s" ""
+	tiles=${tiles// /$CHAR_TILE }
+
+	local y
+	for ((y = 0; y < GRID_HEIGHT; y++)); do
+		tput cup 2 $((y + 1))
+		printf "%b%s%b" "$FG_GRAY" "$tiles" "$FG_RESET"
+	done
+
+	local i
+	for ((i = 0; i < ${#body_x[@]}; i++)); do
+		draw_char "${body_x[$i]}" "${body_y[$i]}" "$CHAR_BODY"
+	done
+	draw_char $head_x $head_y "$CHAR_HEAD"
+	draw_char $apple_x $apple_y "$CHAR_APPLE"
+}
+
+game_over() {
+	tput rmcup # disable the alternative buffer
+	tput cnorm # make cursor visible
+	stty echo  # turn on echoing
+
+	printf "%bgame over%b\n" "$FG_RED" "$FG_RESET"
+	display_score "end"
+}
+
+main() {
+	trap "game_over; printf '\n'" EXIT
+
+	tput smcup # enable the alternative buffer
+	tput civis # make cursor invisible
+	stty -echo # turn off echoing
+
+	CANVAS_WIDTH=$((GRID_WIDTH * 2 + 3))
+	CANVAS_HEIGHT=$((GRID_HEIGHT + 2))
+	OFFSET_X=$(((COLUMNS - CANVAS_WIDTH) / 2))
+	OFFSET_Y=$(((LINES - CANVAS_HEIGHT) / 2))
+	CENTER_X=$(((CANVAS_WIDTH - 29) / 2))
+
+	display_header
+	draw_walls
+	display_footer
+
+	score=0
+	highscore=0
+	init_game
+
+	# game loop
+	while true; do
+		if IFS= read -rn 1 -t $DELAY; then handle_input; fi
+		# wait for input before moving
+		if ((dir_x == 0 && dir_y == 0)); then continue; fi
+
+		prev_head_x=$head_x
+		prev_head_y=$head_y
+		head_x=$((head_x + dir_x))
+		head_y=$((head_y + dir_y))
+
+		# validate new head position before moving the snake
+		if ! validate_move; then
+			read -rn 1
+			init_game
+			handle_input
+			continue
+		fi
+
+		update_snake_pos
+	done
+}
+
+main
